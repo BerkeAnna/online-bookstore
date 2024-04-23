03f28267eee9593ac4deefb1bd64debb6c46d9d6
BerkeAnna <berke.anna0803@gmail.com>
2024-04-23 15:18:22 +0200

    menu

--- bookstore/src/app/app.component.ts
+++ bookstore/src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'bookstore';
   page= 'books';
+
+  changePage(selectedPage: string){
+    this.page = selectedPage;
+  }
 }
--- bookstore/src/app/shared/menu/menu.component.ts
+++ bookstore/src/app/shared/menu/menu.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit{
+
+  @Output() selectedPage: EventEmitter<string> = new EventEmitter();
+
+  ngOnInit(): void {
+    console.log('colled ngoninit')
+  }
+
+  menuSwitch(pageValue: string){
+    this.selectedPage.emit(pageValue);
+  }
 
 }
